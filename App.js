6dc0508415d16bfbb29f5c4c48b7ceaaca447334
sr-garje99 <shubham.garje198@gmail.com>
2023-10-16 23:14:09 +0530

    javascript notes

--- App.js
+++ App.js
@@ -1,4 +1,39 @@
-
+// block scope and shdowing
+var a = 10;
+{
+    //var a =30;
+    let a =20;
+    console.log(a);
+}
+function x(){
+    var a = 100;
+}
+console.log(a);
+//closure
+function outest(){
+    for(var i =0;i<5;i++){
+        function outer(x){
+            setTimeout(function(){
+                console.log(x);
+            },x*1000);
+        }
+        outer(i);
+    }
+}
+//outest();
+//counter
+function counter(){
+    var count =0;
+    function increment(){
+        count++;
+        console.log(count);
+    }
+    return increment
+}
+const out = counter()
+out()
+out()
+counter()
 
         const parent = React.createElement("div",{id:"parent"},
         [ React.createElement("children",{id:"children"},[
