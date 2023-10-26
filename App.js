1b85291f9a50334acc1d226c867d6ee922797148
sr-garje99 <shubham.garje198@gmail.com>
2023-10-26 22:08:05 +0530

    promise

--- App.js
+++ App.js
@@ -60,6 +60,12 @@ function listen(){
     })
 }
 listen();
+//promise
+const GITHUB_API ="https://api.github.com/users/sr-garje99"
+const user = fetch(GITHUB_API);
+user.then((r)=>{
+    console.log(r.data);
+})
 
         const parent = React.createElement("div",{id:"parent"},
         [ React.createElement("children",{id:"children"},[
